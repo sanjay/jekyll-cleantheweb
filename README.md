efe132262be59edc31433e20283e7b9f2b2ab2f3
sanjay <sanjay@laptop>
2014-03-05 23:32:18 -0500

    Multiple features added to make this plugin more functional.

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 A [Liquid tag](http://wiki.shopify.com/Liquid) for [Jekyll](http://jekyllrb.com/) that changes the robot meta header based on the post's age.
 
+I believe that the Internet is littered with old and worthless content that needs to "disappear" from search engines so that more relevant information can rise to the top.  Most of this content comes in the form of blog posts that, while relevant for a certain period of time, quickly lose their value.  Some may argue that generating traffic to a blog/site is more valuable but I believe that this is the wrong type of traffic to acquire.  Why lure users to your site knowing that the content they will find won't be relevant for them.  This plugin attempts to give content creators automatic, fine-grained control over search engine indexing of a site.  Pages will automatically get the "initial" global robot meta directive unless changed in that page's YAML.  Blog posts will start with this same "initial" global meta directive (or the page's "initial" meta directive if set) but after the date hits the page's "activate_date" or the "days_to_age" for the page or globally (in that order of precedence), the page will switch to the "old" meta directive for the page or globally (again, in that order of precedence).
+
 * __Author:__ Sanjay Parekh
 * __Site:__ [http://www.sanjayparekh.com/](http://www.sanjayparekh.com/)
 * __Twitter:__ [@sanjay](https://twitter.com/intent/user?screen_name=sanjay)
@@ -19,10 +21,22 @@ The following parameters are set globally in **_config.yml**.
 
 ```
 cleantheweb:
-  enable:               true
-  new_pages:            "NOARCHIVE, INDEX, FOLLOW"
-  old_pages:            "NOARCHIVE, NOINDEX, FOLLOW"
-  days_to_age:          90
+  enable:       true
+  quiet:        false
+  initial:      "NOARCHIVE, INDEX, FOLLOW"
+  old:          "NOARCHIVE, NOINDEX, FOLLOW"
+  days_to_age:  90
+```
+
+The following parameters can be set on a per page or per post basis in the YAML front matter.
+
+```
+cleantheweb:
+  enable:         true
+  initial:        "NOARCHIVE, INDEX, FOLLOW, SNIPPET"
+  old:            "NOARCHIVE, NOINDEX, FOLLOW, SNIPPET"
+  activate_date:  2014-03-05
+  days_to_age:    120
 ```
 
 ## Usage
--- jekyll-cleantheweb.rb
+++ jekyll-cleantheweb.rb
@@ -1,21 +1,69 @@
 module Jekyll
 
+    # Jekyll-CleanTheWeb v0.1.0
     class CleanTheWebTag < Liquid::Tag
 
-        def initialize(tag_name, text, tokens)
-            return
-        end
-
         def render(context)
-            return "" unless context['site']['cleantheweb']['enable']
-            return "<meta name=\"ROBOTS\" content=\"" + context['site']['cleantheweb']['new_pages'] + "\">" if context['page']['date'].nil?
+            return "<!-- JEKYLL-CLEANTHEWEB CONFIGURATION MISSING -->" unless context['site'].has_key?('cleantheweb')
 
-            activate_date = Date.parse(context['page']['date'].to_s) + context['site']['cleantheweb']['days_to_age']
-            if Date.today > activate_date
-                return "<meta name=\"ROBOTS\" content=\"" + context['site']['cleantheweb']['old_pages'] + "\">"
+            if !context['site']['cleantheweb']['quiet'].nil? && context['site']['cleantheweb']['quiet']
+                result = ""
             else
-                return "<meta name=\"ROBOTS\" content=\"" + context['site']['cleantheweb']['new_pages'] + "\">"
+                result = "<!-- Jekyll-CleanTheWeb v0.1.0 by Sanjay Parekh -->\n"
             end
+
+            # If cleantheweb disabled/false globally or for this page, then return immediately.
+            return result if (context['site']['cleantheweb']['enable'].nil? || !context['site']['cleantheweb']['enable']) || ((context.has_key?('page') && context['page'].has_key?('cleantheweb') && context['page']['cleantheweb'].has_key?('enable')) && !context['page']['cleantheweb']['enable'])
+
+            # If initial is set on the page, that has priority.  Otherwise grab the global value.  If neither exists, set to an empty string.
+            if (context.has_key?('page') && context['page'].has_key?('cleantheweb') && context['page']['cleantheweb'].has_key?('initial'))
+                initial = context['page']['cleantheweb']['initial']
+            elsif (context.has_key?('site') && context['site'].has_key?('cleantheweb') && context['site']['cleantheweb'].has_key?('initial'))
+                initial = context['site']['cleantheweb']['initial']
+            else
+                initial = ""
+            end
+
+            if context.has_key?('page') && context['page']['date'].nil?
+                if initial.nil?
+                    return result
+                else
+                    return result + "<meta name=\"ROBOTS\" content=\"" + initial + "\">"
+                end
+            end
+
+            # If old is set on the page, that has priority.  Otherwise grab the global value.  If neither exists, set to an empty string.
+            if (context.has_key?('page') && context['page'].has_key?('cleantheweb') && context['page']['cleantheweb'].has_key?('old'))
+                old = context['page']['cleantheweb']['old']
+            elsif (context.has_key?('site') && context['site'].has_key?('cleantheweb') && context['site']['cleantheweb'].has_key?('old'))
+                old = context['site']['cleantheweb']['old']
+            else
+                old = ""
+            end
+
+            # If days_to_age is set on the page, that has priority.  Otherwise grab the global value.  If neither exists, set to zero.
+            if (context.has_key?('page') && context['page'].has_key?('cleantheweb') && context['page']['cleantheweb'].has_key?('days_to_age'))
+                days_to_age = context['page']['cleantheweb']['days_to_age'].to_i
+            elsif (context.has_key?('site') && context['site'].has_key?('cleantheweb') && context['site']['cleantheweb'].has_key?('days_to_age'))
+                days_to_age = context['site']['cleantheweb']['days_to_age'].to_i
+            else
+                days_to_age = 0
+            end
+
+            # If date is set on the page, use that as the date to switch instead of calculating based on the days_to_age variable.
+            if (context.has_key?('page') && context['page'].has_key?('cleantheweb') && context['page']['cleantheweb'].has_key?('activate_date'))
+                activate_date = Date.parse(context['page']['cleantheweb']['activate_date'].to_s)
+            else
+                activate_date = Date.parse(context['page']['date'].to_s) + days_to_age
+            end
+
+            if Date.today > activate_date
+                result += "<meta name=\"ROBOTS\" content=\"" + old + "\">" if !old.nil?
+            else
+                result += "<meta name=\"ROBOTS\" content=\"" + initial + "\">" if !initial.nil?
+            end
+
+            return result
 
         end
 
